923825057c5b5684da345bfbd895d62c606110fc
Parmendra-Panwar <panwparmendra7@gamil.com>
2024-06-18 01:46:46 +0530

    Adding Some Validation

--- schema.js
+++ schema.js
@@ -7,6 +7,15 @@ module.exports.listingSchema = Joi.object({
     location: Joi.string().required(),
     country: Joi.string().required(),
     price: Joi.number().required().min(0),
-    image: Joi.string().allow("", null),
+    //image: Joi.string().allow("", null),
+    image: Joi.object({
+      filename: Joi.string().allow("", null).default("hi"),
+      url: Joi.string()
+        .uri()
+        .allow("", null)
+        .default(
+          "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        ),
+    }).default(),
   }).required(),
 });
