ef3d666ff0b05853faa26dfec283c528484454ee
Parmendra-Panwar <panwparmendra7@gamil.com>
2024-10-04 18:49:24 +0530

    few potentioal issues in showing review

--- schema.js
+++ schema.js
@@ -24,6 +24,6 @@ module.exports.listingSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
     rating: Joi.number().min(1).max(5),
-    Comment: Joi.string().required(),
+    comment: Joi.string().required(),
   }).required(),
 })
