1fa0abca66651d2fa3d8a413ea87a7c907678d6e
Parmendra-Panwar <panwparmendra7@gamil.com>
2024-10-10 18:23:26 +0530

    Router.route optemization

--- routes/listing.js
+++ routes/listing.js
@@ -2,45 +2,33 @@ const express = require("express");
 const router = express.Router();
 const validateListing = require("../Validators/listingValidator.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const Listing = require("../models/listing.js");
 const { isloggedIn, isOwner } = require("../Validators/isAthen.js");
 const listingController = require("../controller/listing.js");
 
-//Index Route
-router.get("/", wrapAsync(listingController.index));
-
-// ADD NEW get
-router.get("/new", isloggedIn, wrapAsync(listingController.createNewget));
-//Show Route
-router.get("/:id", wrapAsync(listingController.showListing));
-//new user created
-router.post(
-  "/",
+router.route("/").get(wrapAsync(listingController.index)).post(
   isloggedIn,
   validateListing,
   wrapAsync(listingController.createNewpost)
-);
-//Edit and update
-router.get(
-  "/:id/edit",
+)
+
+router.get("/new", isloggedIn, wrapAsync(listingController.createNewget));
+
+router.route("/:id").get(wrapAsync(listingController.showListing)).delete(
   isloggedIn,
   isOwner,
-  wrapAsync(listingController.editListing)
-);
-//Update
-router.put(
-  "/:id",
+  wrapAsync(listingController.destroy)
+).put(
   isloggedIn,
   isOwner,
   validateListing,
   wrapAsync(listingController.updateListing)
 );
-//delete
-router.delete(
-  "/:id",
+
+router.get(
+  "/:id/edit",
   isloggedIn,
   isOwner,
-  wrapAsync(listingController.destroy)
+  wrapAsync(listingController.editListing)
 );
 
 module.exports = router;
