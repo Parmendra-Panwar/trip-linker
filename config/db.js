524559f503dfeaeeac2c1d5e9de709f352df3eb8
Parmendra-Panwar <panwparmendra7@gamil.com>
2024-10-14 01:01:03 +0530

    V1..

--- config/db.js
+++ config/db.js
@@ -4,11 +4,14 @@ const MONGO_URL = process.env.MONGO_URL;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL, {
+      connectTimeoutMS: 30000, // 30 seconds
+      socketTimeoutMS: 30000, // 30 seconds
+    });
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error("Failed to connect to MongoDB", err);
-    process.exit(1);  // Exit the process with an error code
+    process.exit(1); // Exit the process with an error code
   }
 };
 
