0386fd02c8e7d3960415441afb4e0de37de8714e
Parmendra-Panwar <parmendrapanwar11@gmail.com>
2024-11-12 15:13:12 +0530

    Remove .env file from Git tracking

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 if (process.env.NODE_ENV != "production") {
   require('dotenv').config();
 }
-
+//basic 1 setup
 const express = require("express");
 const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+//
 const ExpressError = require("./utils/ExpressError.js");
 const connectDB = require("./config/db");
+
+//basic 2 setup or better experience 
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")
 const flash = require("connect-flash");
+
+//a @nd a part 1 
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+
+// requring model
 const User = require("./models/user");
 
+//Getting routers
 const listingsRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
 const usersRouter = require("./routes/users.js")
 const authRouter = require("./routes/authrouter.js")
+
 // MongoDB connection
 connectDB();
 
+//mongo store setup for session
 const store = MongoStore.create({
   mongoUrl: process.env.MONGO_URL,
   crypto: {
@@ -49,15 +66,10 @@ const sessionOptions = {
   }
 }
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
 app.use(cookieParser("secretcode"));
 app.use(session(sessionOptions))
 app.use(flash());
+
 //A&A
 app.use(passport.initialize());
 app.use(passport.session());
@@ -69,6 +81,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
+  console.log(req.user)
   res.locals.currUser = req.user;
   next();
 })
